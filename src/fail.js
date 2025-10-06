46953eead9c27da9b4d97c2fcfb491954b0da37d
Tayyib Ismail <94107654+TayyibI@users.noreply.github.com>
2025-10-06 20:55:46 +0100

    Delete src/fail.js

--- src/fail.js
+++
@@ -1,7 +0,0 @@
-const el = document.createElement('button');
-el.popover = 'auto';
-el.showPopover();
-const pattern = new URLPattern('https://example.com/:section');
-if (pattern.test('https://example.com/about')) {
-  console.log('Matches!');
-}
